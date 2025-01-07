3fc9397b0cd61f74164c8c82425f9b40873c3f3a
Subrahmanya Gonella <satishgonella2024@gmail.com>
2025-01-07 10:24:38 +0000

    Initial project structure setup

---
+++ terraform/eks.tf
@@ -0,0 +1,16 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "finops-eks-cluster"
+  cluster_version = "1.25"
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
